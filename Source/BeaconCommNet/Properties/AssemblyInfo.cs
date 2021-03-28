83be9f1a22452a89438bc528bb77efca4a93c70d
David <david.boots@gmail.com>
2021-03-28 00:08:23 -0500

    Upload AssemblyInfo and .csproj files.

---
+++ Source/BeaconCommNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CommNetConstellation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KSP-TaxiService")]
+[assembly: AssemblyProduct("CommNetConstellation")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f844673a-deda-4823-976d-65ceeecb31e4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+// Don't include the patch revision in the AssemblyVersion - as this will break any dependent
+// DLLs any time it changes.  Breaking on a minor revision is probably acceptable - it's
+// unlikely that there wouldn't be other breaking changes on a minor version change.
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0.2")]
+
+// Use KSPAssembly to allow other DLLs to make this DLL a dependency in a
+// non-hacky way in KSP.  Format is (AssemblyProduct, major, minor), and it
+// does not appear to have a hard requirement to match the assembly version.
+[assembly: KSPAssembly("CommNetConstellation", 1, 0)]
